5402bbdd0ef028504ebb0446b5cae2fb48caf3e2
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2022-12-07 20:08:48 -0300

    include then and catch in data.base.helper

--- backend/src/db/database.helper.js
+++ backend/src/db/database.helper.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const databaseUrl = "";
 
 const connectToDataBase = () => {
-  return mongoose.connect(databaseUrl);
+  return mongoose
+    .connect(databaseUrl)
+    .then(() => console.log("Banco de dados conectado com sucesso!"))
+    .catch((error) =>
+      console.log("Erro na conexao com o banco de dados. \n", error)
+    );
 };
 
 const isObjectIdValid = (id) => {
