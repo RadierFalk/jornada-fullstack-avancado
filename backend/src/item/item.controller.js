749f99291a3e2c28c976f3b039743fcc11a2a996
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2022-12-07 19:38:05 -0300

    include mongoose

--- backend/src/item/item.controller.js
+++ backend/src/item/item.controller.js
@@ -1,75 +1,73 @@
+const service = require("./item.service");
 const { isObjectIdValid } = require("../db/database.helper");
 
 const findAll = (req, res) => {
-    const itens = [];
-    res.send(itens);
-
+  const itens = service.findAll();
+  res.send(itens);
 };
 
 const findById = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    if (!isObjectIdValid(id)) {
-        return res.status(400).send({message: "ID inválido!"});
-    }
+  if (!isObjectIdValid(id)) {
+    return res.status(400).send({ message: "ID inválido!" });
+  }
 
-    const item = {};
+  const item = {};
 
-    if (!item) {
-        return res.status(404).send({message: "ID não encontrado!"});
-    }
-    res.send(item);
-}
+  if (!item) {
+    return res.status(404).send({ message: "ID não encontrado!" });
+  }
+  res.send(item);
+};
 
-    const create = (req, res) => {
-        const item = req.body;
+const create = (req, res) => {
+  const item = req.body;
 
-        if (!item || !item.name || !item.imageUrl || !item.category) {
-            return res.status(400).send({message: "Dados invalidos"});
+  if (!item || !item.name || !item.imageUrl || !item.category) {
+    return res.status(400).send({ message: "Dados invalidos" });
+  }
 
-        }
+  const newItem = {};
 
-        const newItem = {};
+  res.status(201).send(newItem);
+};
 
-        res.status(201).send(newItem);
-    };
+const update = (req, res) => {
+  const id = req.params.id;
 
-    const update = (req, res) => {
-        const id = req.params.id;
+  if (!isObjectIdValid(id)) {
+    return res.status(400).send({ message: "Dados inválidos!" });
+  }
 
-        if (!isObjectIdValid(id)) {
-            return res.status(400).send({message: "Dados inválidos!"});
-        }
+  const updateItem = {};
 
-        const updateItem = {};
+  if (!updateItem) {
+    return res.status(404).send({ message: "Item não encontrado!" });
+  }
 
-        if (!updateItem) {
-            return res.status(404).send({message: "Item não encontrado!"});
-        }
+  res.send({ message: "Item atualizado com sucesso!" });
+};
 
-        res.send({message: "Item atualizado com sucesso!"});
-    
-    };
+const deleteById = (req, res) => {
+  const id = req.params.id;
 
-    const deleteById = (req, res) => {
-        const id = req.params.id;
+  if (!isObjectIdValid(id)) {
+    return res.status(400).send({ message: "Item não encontrado!" });
+  }
 
-        if(!isObjectIdValid(id)) {
-            return res.status(400).send({message: "Item não encontrado!"});
-        }
+  const deleteItem = {};
 
-        const deleteItem = {};
-
-        if(!deleteItem) {
-            return res.status(404).send({message: "Item não encontrado!"});
-        }
-        res.send({message: "Item excluído com sucesso!"});
-    };
+  if (!deleteItem) {
+    return res.status(404).send({ message: "Item não encontrado!" });
+  }
+  res.send({ message: "Item excluído com sucesso!" });
+};
 
 module.exports = {
-    findAll,
-    findById,
-    create,
-    update,
-    deleteById,
+  findAll,
+  findById,
+  create,
+  update,
+  deleteById,
 };
